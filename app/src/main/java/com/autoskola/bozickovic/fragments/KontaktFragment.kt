83e39d90b9e51523c41cab00192cea5b548bf98e
BojanDolic <dolicbojan2@gmail.com>
2021-06-03 00:45:44 +0200

    Added libraries for project.
    Implemented navigation component, added fragment_main.xml and fragment_kontakt.xml and connected them with nav component.
    
    MVP design of those two fragments.

---
+++ app/src/main/java/com/autoskola/bozickovic/fragments/KontaktFragment.kt
@@ -0,0 +1,40 @@
+package com.autoskola.bozickovic.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.autoskola.bozickovic.R
+import com.autoskola.bozickovic.databinding.FragmentKontaktBinding
+import com.autoskola.bozickovic.databinding.FragmentMainBinding
+
+
+class KontaktFragment : Fragment() {
+
+    private var _binding: FragmentKontaktBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentKontaktBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        NavigationUI.setupWithNavController(binding.toolbar, findNavController())
+    }
+
+}
---
+++ app/src/main/java/com/autoskola/bozickovic/fragments/MainFragment.kt
@@ -0,0 +1,48 @@
+package com.autoskola.bozickovic.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.autoskola.bozickovic.R
+import com.autoskola.bozickovic.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        NavigationUI.setupWithNavController(binding.toolbar, findNavController())
+
+        binding.kontaktBtn.setOnClickListener {
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToKontaktFragment())
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
